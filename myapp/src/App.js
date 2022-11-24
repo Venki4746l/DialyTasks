400da450209aac4fd5a13d0c2f71ccf8c329184f
venkateswara rao <venkigopisamineni123@gmail.com>
2022-11-24 17:20:02 +0530

    changed

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import StayUpdate from "./Componets/Pages/StayUpdate";
 //import DisplayStore from "./Componets/Store/DisplayStore";
-import CardItems from './Componets/CardItems/CardItems';
+//import CardItems from './Componets/CardItems/CardItems';
+
 
 
 const App = () => {
-  return <div><CardItems/> </div>;
+  return <div><StayUpdate/> </div>;
 };
 
 export default App;
